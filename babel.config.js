3e9a9ab67712d7dcd00eb53bb3e951926b149c33
Rajvardhan135 <rajvardhansinghchib@gmail.com>
2024-06-30 22:18:33 +0530

    icon changed

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     "nativewind/babel",
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
     'react-native-reanimated/plugin',
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
   ],
 };
--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,6 +1,8 @@
 import notifee, { TimestampTrigger, TriggerType } from '@notifee/react-native';
+import { getRandomMessage } from './messages';
 
-export async function createTriggerNotification(startDate, title) {
+export async function sendTaskReminder(startDate, title) {
+    await notifee.requestPermission()
     const notificationTime = new Date(new Date(startDate).getTime() - 5 * 60000);
     console.log('0', notificationTime);
     if (notificationTime <= new Date()) return;
@@ -32,4 +34,31 @@ export async function createTriggerNotification(startDate, title) {
 
 export const cancelNotification = async (id) => {
     await notifee.cancelNotification(`${id}`);
+}
+
+export const sendCongratulatoryMessage = async () => {
+    const timeStamp = new Date().getTime() + 6000
+    const message = getRandomMessage();
+    const trigger = {
+        type: TriggerType.TIMESTAMP,
+        timestamp: timeStamp,
+    };
+    const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+    });
+    await notifee.createTriggerNotification(
+        {
+            id: `timeStamp`,
+            title: 'Tasks Completed!',
+            body: message,
+            android: {
+                channelId,
+                pressAction: {
+                    id: 'default',
+                },
+            },
+        },
+        trigger,
+    );
 }
