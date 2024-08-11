016340c5899c2cb129f7a77d810f1e0dde2997e2
VaibhavSutar <sutarvaibhav37@gmail.com>
2024-08-11 12:03:46 -0600

    pulling new updates 3

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -1,38 +1,42 @@
-import { View, Text, Animated } from 'react-native'
-import React, { useRef, useEffect } from 'react'
-import LottieView from 'lottie-react-native'
-import database from '../watermellon.config'
-import logoImg from '../assets/logo.png'
+import {View, Text, Animated} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import LottieView from 'lottie-react-native';
+import database from '../watermellon.config';
+import logoImg from '../assets/logo.png';
 
-const SplashScreen = ({ navigation }) => {
-    const imageOpacity = useRef(new Animated.Value(0)).current;
+const SplashScreen = ({navigation}) => {
+  const imageOpacity = useRef(new Animated.Value(0)).current;
 
-    const handleAnimationFinish = async () => {
-        Animated.timing(imageOpacity, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-        }).start(async () => {
-            const user = await database.get('profile').query().fetch();
-            if (user.length === 0) {
-                navigation.replace('Create Profile');
-            } else {
-                navigation.replace('Create Profile');
-                // navigation.replace('Bottom Tab');
-            }
-        });
-    };
+  const handleAnimationFinish = async () => {
+    Animated.timing(imageOpacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(async () => {
+      const user = await database.get('profile').query().fetch();
+      if (user.length === 0) {
+        navigation.replace('Create Profile');
+      } else {
+        navigation.replace('Create Profile');
+        // navigation.replace('Bottom Tab');
+      }
+    });
+  };
 
+  useEffect(() => {
+    handleAnimationFinish();
+  }, []);
 
-    useEffect(() => {
-        handleAnimationFinish();
-    }, [])
+  return (
+    <View className="items-center justify-center flex-1 bg-black">
+      <Animated.Image
+        resizeMode="contain"
+        className=""
+        style={{opacity: imageOpacity}}
+        source={logoImg}
+      />
+    </View>
+  );
+};
 
-    return (
-        <View className='items-center justify-center flex-1 bg-black'>
-            <Animated.Image resizeMode="contain" className='' style={{ opacity: imageOpacity }} source={logoImg} />
-        </View>
-    )
-}
-
-export default SplashScreen
+export default SplashScreen;
