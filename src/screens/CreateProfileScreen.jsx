b035fe2c97359e65395a346a98ec87103bb6a713
VaibhavSutar <sutarvaibhav37@gmail.com>
2024-08-14 10:32:35 -0600

    Create Profile: Optional fields added

--- src/screens/CreateProfileScreen.jsx
+++ src/screens/CreateProfileScreen.jsx
@@ -1,17 +1,22 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
 import database from '../watermellon.config';
-import logoImg from '../assets/logo.png'
-import { TextInputField, NumberInputField, DateInputField, PhoneInputField } from '../components/FormFields';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import logoImg from '../assets/logo.png';
+import {
+  TextInputField,
+  NumberInputField,
+  DateInputField,
+  PhoneInputField,
+} from '../components/FormFields';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-const CreateProfileScreen = ({ navigation }) => {
+const CreateProfileScreen = ({navigation}) => {
   const [name, setName] = useState('');
   const [isNameValid, setIsNameValid] = useState(true);
   const [profession, setProfession] = useState('');
   const [isProfessionValid, setIsProfessionValid] = useState(true);
-  const [dob, setDob] = useState('')
-  const [isDobValid, setIsDobValid] = useState(true)
+  const [dob, setDob] = useState('');
+  const [isDobValid, setIsDobValid] = useState(true);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [cca2, setCca2] = useState('');
   const [callingCode, setCallingCode] = useState('');
@@ -20,11 +25,19 @@ const CreateProfileScreen = ({ navigation }) => {
 
   const saveUser = async () => {
     if (name.length === 0) setIsNameValid(false);
-    if (profession.length === 0) setIsProfessionValid(false);
-    if (phoneNumber.length === 0 || cca2.length === 0) setIsPhoneNumberValid(false);
-    if (dob.length === 0) setIsDobValid(false);
+    // if (profession.length === 0) setIsProfessionValid(false);
+    // if (phoneNumber.length === 0 || cca2.length === 0)
+    //   setIsPhoneNumberValid(false);
+    // if (dob.length === 0) setIsDobValid(false);
 
-    if (name.length === 0 || profession.length === 0 || phoneNumber.length === 0 || dob.length === 0 || cca2.length === 0) {
+    if (
+      name.length === 0
+      // ||
+      // profession.length === 0 ||
+      // phoneNumber.length === 0 ||
+      // dob.length === 0 ||
+      // cca2.length === 0
+    ) {
       return;
     }
 
@@ -33,13 +46,21 @@ const CreateProfileScreen = ({ navigation }) => {
       if (existingUser.length > 0) {
         await Promise.all(existingUser.map(user => user.destroyPermanently()));
       }
-      await database.get('profile').create(user => {
-        user.name = name;
-        user.profession = profession;
-        user.phoneNumber = phoneNumber;
-        user.countryCode = selectedCountry.cca2;
-        user.dob = dob;
-      })
+      await database
+        .get('profile')
+        .create(user => {
+          user.name = name;
+          if (profession.length > 0) {
+            user.profession = profession;
+          }
+          if (phoneNumber.length > 0 && cca2.length > 0) {
+            user.phoneNumber = phoneNumber;
+            user.countryCode = selectedCountry.cca2;
+          }
+          if (dob.length > 0) {
+            user.dob = dob;
+          }
+        })
         .then(() => navigation.replace('Bottom Tab'))
         .catch(err => {
           console.log(name, profession, phoneNumber, selectedCountry.cca2, dob);
@@ -49,22 +70,35 @@ const CreateProfileScreen = ({ navigation }) => {
   };
 
   return (
-    <View className='justify-end flex-1 bg-[#242424]'>
-      <View className='flex-row items-center justify-center h-[35%]'>
-        <Image className='mb-10' source={logoImg} />
+    <View className="justify-end flex-1 bg-[#242424]">
+      <View className="flex-row items-center justify-center h-[35%]">
+        <Image className="mb-10" source={logoImg} />
       </View>
-      <View className='w-full h-[65%] pt-10 bg-white'>
+      <View className="w-full h-[65%] pt-10 bg-white">
         <KeyboardAwareScrollView
-          className='flex-1 px-12'
+          className="flex-1 px-12"
           enableOnAndroid={true}
           keyboardShouldPersistTaps="handled"
-          extraScrollHeight={10}
-        >
-          <View className='w-full gap-4'>
-            <Text className='mb-4 text-3xl font-bold text-black'>Create Profile</Text>
-            <View className='w-full'>
-              <TextInputField isValid={isNameValid} setIsValid={setIsNameValid} value={name} setValue={setName} name='Name' />
-              <TextInputField isValid={isProfessionValid} setIsValid={setIsProfessionValid} value={profession} setValue={setProfession} name='Profession' />
+          extraScrollHeight={10}>
+          <View className="w-full gap-4">
+            <Text className="mb-4 text-3xl font-bold text-black">
+              Create Profile
+            </Text>
+            <View className="w-full">
+              <TextInputField
+                isValid={isNameValid}
+                setIsValid={setIsNameValid}
+                value={name}
+                setValue={setName}
+                name="Name"
+              />
+              <TextInputField
+                isValid={isProfessionValid}
+                setIsValid={setIsProfessionValid}
+                value={profession}
+                setValue={setProfession}
+                name="Profession"
+              />
               <PhoneInputField
                 isDarkTheme={false}
                 selectedCountry={selectedCountry}
@@ -78,11 +112,20 @@ const CreateProfileScreen = ({ navigation }) => {
                 callingCode={callingCode}
                 setCallingCode={setCallingCode}
               />
-              <DateInputField value={dob} setValue={setDob} isValid={isDobValid} setIsValid={setIsDobValid} name='Date of birth' maxDate={new Date()} />
+              <DateInputField
+                value={dob}
+                setValue={setDob}
+                isValid={isDobValid}
+                setIsValid={setIsDobValid}
+                name="Date of birth"
+                maxDate={new Date()}
+              />
             </View>
-            <View className='w-full'>
-              <TouchableOpacity onPress={saveUser} className='w-full p-3 rounded-md my-10 bg-[#424242] items-center'>
-                <Text className='text-white'>Save Profile</Text>
+            <View className="w-full">
+              <TouchableOpacity
+                onPress={saveUser}
+                className="w-full p-3 rounded-md my-10 bg-[#424242] items-center">
+                <Text className="text-white">Save Profile</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -90,6 +133,6 @@ const CreateProfileScreen = ({ navigation }) => {
       </View>
     </View>
   );
-}
+};
 
 export default CreateProfileScreen;
